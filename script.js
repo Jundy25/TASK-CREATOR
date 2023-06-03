fc1618946b8fe2f4b1334efb524ca1ec3119d284
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2023-06-03 13:25:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
+
+
+
 function add(){
+    
     var btn = document.getElementById('int').value;
     if (btn == ""){
         alert("Please Enter a Task!");
         return false;
     }
     else{
-        const con = document.querySelector('.content');
-        const addition = document.createElement('div');
+
+        
+
+
+        document.getElementById('int').value = "";
+        let con = document.querySelector('.content');
+        let addition = document.createElement('div');
         addition.className = 'addition';
         addition.id = 'task-cont';
         con.append(addition);
 
-        const desc = document.createElement('div');
+        let desc = document.createElement('div');
 
         desc.className = 'desc';
         desc.id = 'desc';
         desc.innerText = btn;
         addition.append(desc);
 
-        const buttons = document.createElement('div');
+        let buttons = document.createElement('div');
 
         buttons.className = 'buttons';
         buttons.id = 'buttons';
 
 
-        const done = document.createElement('button');
+        let done = document.createElement('button');
 
+        
         done.className = 'done';
         done.id = 'done';
         done.innerText = 'Done';
         buttons.append(done);
-
-        const del = document.createElement('button');
+        let del = document.createElement('button');
 
         del.className = 'del';
         del.id = 'del';
@@ -39,14 +48,33 @@ function add(){
         buttons.append(del);
 
         addition.append(buttons);
+
+
+        
+        
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+              var div = this.parentElement.parentElement;
+              div.remove();
+            }
+        }
+        var list = done;
+        list.addEventListener('click', function myFunction() {
+            list.parentElement.previousElementSibling.classList.toggle("crash");
+            list.classList.toggle('clicked');
+          });
+        
     }
-
 }
-const element = document.getElementById("del");
-element.addEventListener("click", myFunction);
 
-element.addEventListener("click", myFunction);
 
-function myFunction() {
-  document.getElementById("task-cont").remove();
+
+var close = document.getElementsByClassName("del");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement.parentElement;
+    div.remove();
+  }
 }
+
